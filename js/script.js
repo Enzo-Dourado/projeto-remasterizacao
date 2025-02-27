72778ed4803d917181fafdc4f3f5048621b20f3a
Enzo Dourado <enzodourado.cripto@gmail.com>
2025-02-27 11:12:59 -0300

    Js externo

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var nome = prompt('Qual seu nome?')
+window.alert('Seja Bem-vindo(a), ' + nome)
+
+
+function clicMenu() {
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+    }
+    else {
+        menu.style.display = 'block'
+    }
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'block'
+    }
+    else {
+        menu.style.display = 'none'
+    }
+}

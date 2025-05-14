a8ed4c71e8dbd878823672da04f30c90ae9d555f
Enzo Dourado <enzodourado.cripto@gmail.com>
2025-05-14 20:13:47 -0300

    Organizando e Ajustando projeto

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+
+function clicMenu() {
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+    }
+    else {
+        menu.style.display = 'block'
+    }
+}
+
+// function alerta() {
+//     var saudacao = prompt('Qual seu nome?')
+//     var alerta = alert(`Seja bem-vindo(a), ${saudacao}`)
+// }
+
+// function mudouTamanho() {
+//     if (window.innerWidth >= 768) {
+//         menu.style.display = 'block'
+//     }
+//     else {
+//         menu.style.display = 'none'
+//     }
+// }
